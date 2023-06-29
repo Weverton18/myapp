b8b79265f6ea3b6e7cb25363b4900ff1f9e1f4c4
Weverton18 <wevertonardo17@gmail.com>
2023-06-28 21:54:16 -0300

    Learning class 7 and 8 blade directives

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,17 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $users = User::all();
+        return view('users', [
+            'users' => $users
+        ]);
+    }
 
-    public function show(User $user, Request $request){
-        dd($request);
-        return $user;
+    public function show(User $user){
+        return view('user', [
+            'name' => 'Jon Snow',
+            'user' => $user
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use App\Http\Controllers\UserController;
-use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,58 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/request', function(Request $request){
-    // $r = $request->all();
-    // $r = $request->query('keyword');
-    // $r = $request->input('keyword');
-    // $r = $request->path();
-    // $r = $request->url();
-    // $r = $request->fullUrl();
-    // $r = $request->header();
-    // $r = $request->whenHas('keyword', function($input){
-    //     dd('x', $input);
-    // });
+Route::get('user/{user}', [UserController::class, 'show'])->name('user.show');
+Route::get('users', [UserController::class, 'index'])->name('user.index');
 
-    // $r = $request->whenFilled('keyword', function($input){
-    //     dd('x', $input);
-    // });
 
-    // if($r){
-    //     dd('Faça alguma coisa!');
-    // }
-
-    $r = $request->ip();
-
-    dd($r);
-    return 'x';
-});
-
-Route::get('user/{user}', [UserController::class, 'show']);
-
-// Route::get('user/{user:email}', function(User $user){
-//     return $user;
-// });
-
-Route::prefix('usuarios')->group(function(){
-    Route::get('', function (){
-        return 'usuarios';
-    })->name('usuarios');
-
-    Route::get('/{id}', function (){
-        return 'Mostrar detalhes';
-    })->name('usuarios.show');
-
-    Route::get('/{id}/tags', function (){
-        return 'Tags do Usuário';
-    })->name('usuarios.tags');
-});
-
-Route::get('/a-empresa/{string?}', function ($string = null) {
-    return $string;
-    // return view('welcome');
-})->name('a-empresa');
-
-Route::get('/users/{paramA}/{paramB}', function ($paramB, $paramA) {
-    return $paramA .' - '.$paramB;
-    // return view('welcome');
-});
+Route::get('/', function () {
+    // return view('teste');
+    return view('welcome');
+})->name('home');
