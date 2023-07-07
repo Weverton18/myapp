71f9c09e7120b39a0152b000e1a50686f9f0fbf9
Weverton18 <wevertonardo17@gmail.com>
2023-07-07 10:16:26 -0300

    Learning class 16 Eloquent ORM - updating data

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -8,11 +8,26 @@ use Illuminate\Http\Request;
 class BusinessController extends Controller
 {
     public function index(){
-        $business = Business::create([
-            'name' => 'Wevertons EI',
-            'email' => 'weverton@dominio.com',
-            'address' => 'Rua a quadra b'
-        ]);
+        // $business = Business::find(5)->update([
+        //     'name' => 'Teste',
+        //     'email' => 'dasdasd@dominio.com'
+        // ]);
+
+        // $business = Business::find(5);
+        // $business->name = 'Jon Snow';
+        // $business->email = 'jonsnow.com';
+        // $business->address = 'Quadra c rua B';
+
+        // $business->save();
+
+
+        $input = [
+            'name' => 'Leticia',
+            'email' => 'let@dominio.com'
+        ];
+        $business = Business::find(5);
+        $business->fill($input);
+        $business->save();
 
         dd($business);
     }
