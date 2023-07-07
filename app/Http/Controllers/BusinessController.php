6846bfbf552fb2aaf4d0baa0c8088d75bb937dea
Weverton18 <wevertonardo17@gmail.com>
2023-07-07 10:39:02 -0300

    Learnig class 17 Eloquent ORM - deleting data

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -7,28 +7,12 @@ use Illuminate\Http\Request;
 
 class BusinessController extends Controller
 {
-    public function index(){
-        // $business = Business::find(5)->update([
-        //     'name' => 'Teste',
-        //     'email' => 'dasdasd@dominio.com'
-        // ]);
-
-        // $business = Business::find(5);
-        // $business->name = 'Jon Snow';
-        // $business->email = 'jonsnow.com';
-        // $business->address = 'Quadra c rua B';
-
-        // $business->save();
-
-
-        $input = [
-            'name' => 'Leticia',
-            'email' => 'let@dominio.com'
-        ];
-        $business = Business::find(5);
-        $business->fill($input);
-        $business->save();
-
-        dd($business);
+    public function index()
+    {
+        \DB::connection()->enableQueryLog();
+        $business = Business::Where('name', 'LIKE', '%weverton%')->get();
+        $query = \DB::getQueryLog();
+        dd($query);
+        // $business->delete();
     }
 }
